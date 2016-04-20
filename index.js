44f9551e58af2c38ff47551e7bc65a94cd422bd2
BOOLRon <ron.zhang.36@gmail.com>
2016-04-20 18:51:08 +0800

    mod: must input config file path as paramter

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 #!/usr/local/bin/node
 
-var path = require('path');
+var path = require('path-extra');
 var chokidar = require('chokidar');
 var fs = require('fs');
 var cloudinary = require('cloudinary')
-var log = console.log.bind(console);
+var prettyjson = require('prettyjson');
 require('shelljs/global');
-var syncDirPath = path.resolve(__dirname, '..','..','..','Desktop/up2cloudinary');
+var log = console.log.bind(console);
+var userRootPath = path.homedir();
+var syncDirPath = path.resolve(userRootPath,'Desktop','up2cloudinary');
+var confPath = path.resolve(userRootPath,'cloudinary.conf');
+console.log(syncDirPath)
+console.log(confPath)
+
+
+if (!fs.existsSync(confPath)){
+    console.warn("you must set up a JSONfile names 'cloudinary.conf' in your ~ path");
+    logExampleJSON();
+    progress.exit();
+}
+
+function logExampleJSON(){
+  console.warn("Example:")
+    var exampleJSON = {
+      cloud_name: 'your cloud name',
+      api_key: 'your api key',
+      api_secret: 'your api secret'
+    };
+    console.log(prettyjson.render(exampleJSON, {
+      noColor: false
+    }));
+}
+
+var configDic = JSON.parse(fs.readFileSync(confPath,'utf8'))
+
+if (configDic) {
+    cloudinary.config(configDic);
+}else {
+    console.warn("config file format errorh");
+    logExampleJSON();
+    progress.exit();
+}
 
 if (!fs.existsSync(syncDirPath)){
     fs.mkdirSync(syncDirPath);
@@ -14,14 +48,6 @@ if (!fs.existsSync(syncDirPath)){
 
 exec('defaults write com.apple.screencapture location' + syncDirPath)
 exec('killall SystemUIServer')
-
-var configDic = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'cloudinary.conf'),'utf8'))
-
-if (configDic) {
-    cloudinary.config(configDic);
-}else {
-    console.warn("you must make a file names 'cloudinary.conf'");
-}
 
 function performUploadByFilePath(filePath){
     log(`File ${filePath} has been added`);
