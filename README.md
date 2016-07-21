54f338b283ea3d2ec3596e181e8d10054457a753
BOOLRon <ron.zhang.36@gmail.com>
2016-07-21 16:41:07 +0800

    support copy to paste after upload success

--- README.md
+++ README.md
@@ -44,25 +44,6 @@ killall SystemUIServer
 watchdog4cloudinary
 ```
 
-每次触发Mac系统截屏 (快捷键`cmd`+`shift`+`4`)，都会在`~/Destop/up2cloudinary`目录下的生成并上传[cloudianry](http://cloudinary.com/)，并返回：
+每次触发Mac系统截屏 (快捷键`cmd`+`shift`+`4`)，都会在`~/Destop/up2cloudinary`目录下的生成并上传[cloudianry](http://cloudinary.com/)，上传成功后会自动复制链接到剪切板
 
-
-```
-{ public_id: 'bgdomzedck7n4i4t794w',
-  version: 1461150597,
-  signature: '637f38cd2db14dca9dda8e3d0459b19e9152fbde',
-  width: 164,
-  height: 133,
-  format: 'png',
-  resource_type: 'image',
-  created_at: '2016-04-20T11:09:57Z',
-  tags: [],
-  bytes: 1523,
-  type: 'upload',
-  etag: 'b3a1ed736941c8dbee1649bbc4b2c5e6',
-  url: 'http://res.cloudinary.com/boolron/image/upload/v1461150597/bgdomzedck7n4i4t794w.png',
-  secure_url: 'https://res.cloudinary.com/boolron/image/upload/v1461150597/bgdomzedck7n4i4t794w.png',
-  original_filename: 'Screen Shot 2016-04-20 at 7.09.53 PM' 
-}
-```
 
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/local/bin/node
 
+var notify = require('osx-notifier');
+var ncp = require('copy-paste');
 var path = require('path-extra');
 var chokidar = require('chokidar');
 var fs = require('fs');
@@ -50,9 +52,21 @@ function performUploadByFilePath(filePath){
     log(`File ${filePath} has been added`);
 
     // File uploader
-    cloudinary.uploader.upload(filePath,function(err,image){
-      console.log("** File Upload");
-      if (err){ console.warn(err);}
+    cloudinary.uploader.upload(filePath,function(result){
+        var secure_url = result.secure_url;
+        if(secure_url){
+            ncp.copy(secure_url,function(){
+                notify({
+                    type: 'pass',
+                    title: '图床',
+                    subtitle: '截图上传成功',
+                    message: '链接已复制到剪切板',
+                    group: 'watchdog4cloudinary',
+                });
+            });
+        }else {
+
+        }
       fs.unlinkSync(filePath);
     });
 
